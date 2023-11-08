13a508a1c3d7efcb4299f5eb925c4495b2e63fb4
Jaguvaari <dteoi001@edu.xamk.fi>
2023-11-08 11:43:10 +0200

    Coin collection event

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -7,10 +7,22 @@ public class CoinPickup : MonoBehaviour
 {
     public static event Action<CoinPickup> OnCoinCollected;
 
+    public int CoinValue { get; private set; }
+
+    public void SetValue(int newValue)
+    {
+        CoinValue = newValue;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnDestroy()
+    {
+        OnCoinCollected?.Invoke(this);
     }
 
     // Update is called once per frame
