d31d71c28854d25e30f5f6fee63ed31ea384c1de
TeroOinonen <dteoi001@edu.xamk.fi>
2023-11-23 09:54:31 +0200

    Buggy double buffer

--- Assets/Scripts/GameOfLife/Game.cs
+++ Assets/Scripts/GameOfLife/Game.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class Game : MonoBehaviour
@@ -11,6 +12,11 @@ public class Game : MonoBehaviour
     private static readonly int Height = 10;
 
     bool[,] grid = new bool[Width, Height];
+    bool[,] gridB = new bool[Width, Height];
+
+    bool[,] currentGrid;
+    bool[,] nextGrid;
+
     GameObject[,] tiles = new GameObject[Width, Height];
 
     private float TimeAccu = 0.0f;
@@ -29,6 +35,8 @@ public class Game : MonoBehaviour
             {
                 // Clear the grid
                 grid[x, y] = false;
+                gridB[x, y] = false;
+
                 // Instantiate the tile
                 tiles[x, y] = Instantiate(TilePrefab, new Vector3(x, 0, y) * 1.05f, Quaternion.identity);
 
@@ -37,7 +45,7 @@ public class Game : MonoBehaviour
             }
         }
 
-        grid[3,5] = true;
+        grid[3, 5] = true;
         grid[4, 5] = true;
         grid[5, 5] = true;
         grid[4, 4] = true;
@@ -46,6 +54,9 @@ public class Game : MonoBehaviour
         tiles[4, 5].GetComponentInChildren<MeshRenderer>().material.color = Color.white;
         tiles[5, 5].GetComponentInChildren<MeshRenderer>().material.color = Color.white;
         tiles[4, 4].GetComponentInChildren<MeshRenderer>().material.color = Color.white;
+
+        currentGrid = grid;
+        nextGrid = gridB;
     }
 
     // Update is called once per frame
@@ -75,28 +86,50 @@ public class Game : MonoBehaviour
                 // 3. more than 3 live neighbors --> die
                 // 4. dead cell with 3 live neighbors --> rebirth
 
-                int numNeighbors = CountNeighbors(ref grid, x, y);
+                int numNeighbors = CountNeighbors(ref currentGrid, x, y);
 
                 if (numNeighbors < 2 || numNeighbors > 3) // 1 && 3
                 {
-                    grid[x, y] = false;
+                    nextGrid[x, y] = false;
                 }
-                else if (grid[x, y] == true && (numNeighbors == 2 || numNeighbors == 3)) // 2
+                else if (nextGrid[x, y] == true && (numNeighbors == 2 || numNeighbors == 3)) // 2
                 {
-                    grid[x, y] = true;
+                    nextGrid[x, y] = true;
                 }
                 else if (numNeighbors == 3) // 4
                 {
-                    grid[x, y] = true;
+                    nextGrid[x, y] = true;
+                }
+                else
+                {
+                    Debug.Log("Fell through with x:" + x + " y:" + y);
+                    nextGrid[x, y] = false; // Fall through default, should not really happen
                 }
             }
         }
 
+        DrawGridOnScreen(ref nextGrid);
+
+        TimeAccu = 3; // reset tick timer
+
+        SwapGrids();
+    }
+
+    private void SwapGrids()
+    {
+        bool[,] temp = nextGrid;
+
+        nextGrid = currentGrid;
+        currentGrid = temp;        
+    }
+
+    private void DrawGridOnScreen(ref bool[,] drawGrid)
+    {
         for (int x = 0; x < Width; x++)
         {
             for (int y = 0; y < Height; y++)
             {
-                if (grid[x, y] == true)
+                if (drawGrid[x, y] == true)
                 {
                     tiles[x, y].GetComponentInChildren<MeshRenderer>().material.color = Color.white;
                 }
@@ -106,8 +139,6 @@ public class Game : MonoBehaviour
                 }
             }
         }
-
-        TimeAccu = 3; // reset tick timer
     }
 
     private int CountNeighbors(ref bool[,] grid, int x, int y)
